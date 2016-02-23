ea972c02fc3ee311769197307fe301a4e8ca79ad
Jacob B. Cholewa <jbec@itu.dk>
2016-02-23 13:13:43 +0100

    Added alternative Option implementation

--- 030/exercises.scala
+++ 030/exercises.scala
@@ -97,135 +97,161 @@ object Tree {
 
 sealed trait Option[+A] 
 {
-  def map[B] (f: A=>B) : Option[B] = this match {
-    case None => None
-    case Some(x) => Some(f(x))
-  }
-
-  def getOrElse[B >: A] (default: => B) :B = this match {
-    case Some(x) => x
-    case None => default
-  }
-
-  def flatMap[B] (f: A=>Option[B]) : Option[B] = this match {
-    case None => None
-    case Some(x) => f(x)
-  }
-
-  def orElse[B >: A] (ob : => Option[B]) : Option[B] = this match {
-    case None => ob
-    case Some(x) => Some(x)
-  }
-  def filter (f: A => Boolean) : Option[A] = this match {
-    case Some(x) => if(f(x)) Some(x) else None
-    case None => None
-  }
+  def map[B] (f: (A) => B) : Option[B]
+  def getOrElse[B >: A] (default: => B) :B
+  def flatMap[B] (f: (A) => Option[B]) : Option[B]
+  def orElse[B >: A] (ob : => Option[B]) : Option[B]
+  def filter (f: (A) => Boolean) : Option[A] 
 }
-    case class Some[+A] (get: A) extends Option[A] 
-    case object None extends Option[Nothing] 
+case class Some[+A] (get: A) extends Option[A] 
+{
+  override def map[B] (f: (A) => B) : Option[B] = Some(f(get))
+  override def getOrElse[B >: A] (default: => B) :B = get 
+  override def flatMap[B] (f: (A) => Option[B]) : Option[B] = f(get)
+  override def orElse[B >: A] (ob : => Option[B]) : Option[B] = this 
+  override def filter (f: (A) => Boolean) : Option[A] = if(f(get)) this else None
+}
+case object None extends Option[Nothing] 
+{
+  override def map[B] (f: (Nothing) => B) : Option[B] = None
+  override def getOrElse[B >: Nothing] (default: => B) :B = default
+  override def flatMap[B] (f: (Nothing) => Option[B]) : Option[B] = None
+  override def orElse[B >: Nothing] (ob : => Option[B]) : Option[B] = ob
+  override def filter (f: (Nothing) => Boolean) : Option[Nothing] = None 
+}
 
-    object ExercisesOption {
 
-      // Remember that mean is implemented in Chapter 4 of the text book
+//sealed trait Option[+A] 
+//{
+//  def map[B] (f: A=>B) : Option[B] = this match {
+//    case None => None
+//    case Some(x) => Some(f(x))
+//  }
+//
+//  def getOrElse[B >: A] (default: => B) :B = this match {
+//    case Some(x) => x
+//    case None => default
+//  }
+//
+//  def flatMap[B] (f: A=>Option[B]) : Option[B] = this match {
+//    case None => None
+//    case Some(x) => f(x)
+//  }
+//
+//  def orElse[B >: A] (ob : => Option[B]) : Option[B] = this match {
+//    case None => ob
+//    case Some(x) => Some(x)
+//  }
+//  def filter (f: A => Boolean) : Option[A] = this match {
+//    case Some(x) => if(f(x)) Some(x) else None
+//    case None => None
+//  }
+//}
+//case class Some[+A] (get: A) extends Option[A] 
+//case object None extends Option[Nothing]
 
-      def mean(xs: Seq[Double]): Option[Double] =
-        if (xs.isEmpty) None
-        else Some(xs.sum / xs.length)
+object ExercisesOption {
 
-        // Exercise 8 (4.2) I know that this is correct; I'am however getting an annoying type error that I can't figure out how to resolve.
+  // Remember that mean is implemented in Chapter 4 of the text book
 
-        def variance (xs: Seq[Double]) : Option[Double] = { 
-          val m : Option[Double] = mean(xs)
-          //val u : Seq[Double] = xs.flatMap(x => m.map(y => math.pow(x-y,2)))
-          val u : Seq[Double] = for(x <- xs; y <- m) yield math.pow(x-y,2)
-          mean (u)
-        }
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
 
-        // Exercise 9 (4.3)
+    // Exercise 8 (4.2) I know that this is correct; I'am however getting an annoying type error that I can't figure out how to resolve.
 
-        def map2[A,B,C] (ao: Option[A], bo: Option[B]) (f: (A,B) => C) :Option[C] = for(a <- ao; b <- bo) yield f(a,b)
-
-        // Exercise 10 (4.4)
-
-        def sequence[A] (aos: List[Option[A]]) : Option[List[A]] = aos.foldRight[Option[List[A]]] (Some(List())) ((e,acc) => acc.flatMap(list => e.map(a => a::list)))
-        // Exercise 11 (4.5)
-
-        def traverse[A,B] (as: List[A]) (f :A => Option[B]) :Option[List[B]] = as match {
-          case x::xs => (traverse (xs) (f)).flatMap(list => f(x).map(a => a::list))
-          case Nil => Some(List())
-        }
-
+    def variance (xs: Seq[Double]) : Option[Double] = { 
+      val m : Option[Double] = mean(xs)
+      //val u : Seq[Double] = xs.flatMap(x => m.map(y => math.pow(x-y,2)))
+      val u : Seq[Double] = for(x <- xs; y <- m) yield math.pow(x-y,2)
+      mean (u)
     }
 
+    // Exercise 9 (4.3)
 
-    // Test cases for running in the compiled vesion (uncomment as you go, or paste
-    // them into REPL in the interactive version)
+    def map2[A,B,C] (ao: Option[A], bo: Option[B]) (f: (A,B) => C) :Option[C] = for(a <- ao; b <- bo) yield f(a,b)
 
-    object Tests extends App {
+    // Exercise 10 (4.4)
 
-      // Exercise 1
-      val p = new java.awt.Point(0,1) with OrderedPoint
-      val q = new java.awt.Point(0,2) with OrderedPoint
-      assert(p < q)
+    def sequence[A] (aos: List[Option[A]]) : Option[List[A]] = aos.foldRight[Option[List[A]]] (Some(List())) ((e,acc) => acc.flatMap(list => e.map(a => a::list)))
+    // Exercise 11 (4.5)
 
-      // Notice how we are using nice infix comparison on java.awt
-      // objects that were implemented way before Scala existed :) (And without the
-      // library implementing a suitable comparator). We did not have to recompile
-      // java.awt.Point
-
-
-      // Exercise 2
-      assert (Tree.size (Branch(Leaf(1), Leaf(2))) == 3)
-
-      // Exercise 3
-      assert (Tree.maximum (Branch(Leaf(1), Leaf(2))) == 2)
-
-      // Exercise 4
-      val t4 = Branch(Leaf(1), Branch(Branch(Leaf(2),Leaf(3)),Leaf(4)))
-      assert (Tree.depth (t4) == 3)
-
-      // Exercise 5
-      val t5 = Branch(Leaf("1"), Branch(Branch(Leaf("2"),Leaf("3")),Leaf("4")))
-      assert (Tree.map (t4) (_.toString) == t5)
-
-      // Exercise 6
-      assert (Tree.size1 (Branch(Leaf(1), Leaf(2))) == 3)
-      assert (Tree.maximum1 (Branch(Leaf(1), Leaf(2))) == 2)
-      assert (Tree.depth1 (t4) == 3)
-      assert (Tree.map1 (t4) (_.toString) == t5)
-
-      // Exercise 7
-      assert (Some(1).map (x => x +1) == Some(2))
-      assert (Some(41).getOrElse(42) == 41)
-      assert (None.getOrElse(42) == 42)
-      assert (Some(1).flatMap (x => Some(x+1)) == Some(2))
-      assert ((None: Option[Int]).flatMap[Int] (x => Some(x+1)) == None)
-      assert (Some(41).orElse (Some(42)) == Some(41))
-      assert (None.orElse (Some(42)) == Some(42))
-      assert (Some(42).filter(_ == 42) == Some(42))
-      assert (Some(41).filter(_ == 42) == None)
-      assert ((None: Option[Int]).filter(_ == 42) == None)
-
-      // Exercise 8
-      //assert (ExercisesOption.variance (List(42,42,42)) == Some(0.0))
-      //assert (ExercisesOption.variance (List()) == None)
-
-
-      // Exercise 9
-      assert (ExercisesOption.map2 (Some(42),Some(7)) (_ + _) == Some(49))
-      assert (ExercisesOption.map2 (Some(42),None) (_ + _) == None)
-      assert (ExercisesOption.map2 (None: Option[Int],Some(7)) (_ + _) == None)
-      assert (ExercisesOption.map2 (None: Option[Int],None) (_ + _) == None)
-
-      // Exercise 10
-      assert (ExercisesOption.sequence (List(Some(1), Some(2), Some(42))) == Some(List(1,2,42)))
-      assert (ExercisesOption.sequence (List(None,    Some(2), Some(42))) == None)
-      assert (ExercisesOption.sequence (List(Some(1), None,    Some(42))) == None)
-      assert (ExercisesOption.sequence (List(Some(1), Some(2), None    )) == None)
-
-      // Exercise 11
-      def f (n: Int) :Option[Int] = if (n%2 == 0) Some(n) else None
-      assert (ExercisesOption.traverse (List(1,2,42)) (Some(_)) == Some(List(1,2,42)))
-      assert (ExercisesOption.traverse (List(1,2,42)) (f) == None)
-
+    def traverse[A,B] (as: List[A]) (f :A => Option[B]) :Option[List[B]] = as match {
+      case x::xs => (traverse (xs) (f)).flatMap(list => f(x).map(a => a::list))
+      case Nil => Some(List())
     }
+
+}
+
+
+// Test cases for running in the compiled vesion (uncomment as you go, or paste
+// them into REPL in the interactive version)
+
+object Tests extends App {
+
+  // Exercise 1
+  val p = new java.awt.Point(0,1) with OrderedPoint
+  val q = new java.awt.Point(0,2) with OrderedPoint
+  assert(p < q)
+
+  // Notice how we are using nice infix comparison on java.awt
+  // objects that were implemented way before Scala existed :) (And without the
+  // library implementing a suitable comparator). We did not have to recompile
+  // java.awt.Point
+
+
+  // Exercise 2
+  assert (Tree.size (Branch(Leaf(1), Leaf(2))) == 3)
+
+  // Exercise 3
+  assert (Tree.maximum (Branch(Leaf(1), Leaf(2))) == 2)
+
+  // Exercise 4
+  val t4 = Branch(Leaf(1), Branch(Branch(Leaf(2),Leaf(3)),Leaf(4)))
+  assert (Tree.depth (t4) == 3)
+
+  // Exercise 5
+  val t5 = Branch(Leaf("1"), Branch(Branch(Leaf("2"),Leaf("3")),Leaf("4")))
+  assert (Tree.map (t4) (_.toString) == t5)
+
+  // Exercise 6
+  assert (Tree.size1 (Branch(Leaf(1), Leaf(2))) == 3)
+  assert (Tree.maximum1 (Branch(Leaf(1), Leaf(2))) == 2)
+  assert (Tree.depth1 (t4) == 3)
+  assert (Tree.map1 (t4) (_.toString) == t5)
+
+  // Exercise 7
+  assert (Some(1).map (x => x +1) == Some(2))
+  assert (Some(41).getOrElse(42) == 41)
+  assert (None.getOrElse(42) == 42)
+  assert (Some(1).flatMap (x => Some(x+1)) == Some(2))
+  assert ((None: Option[Int]).flatMap[Int] (x => Some(x+1)) == None)
+  assert (Some(41).orElse (Some(42)) == Some(41))
+  assert (None.orElse (Some(42)) == Some(42))
+  assert (Some(42).filter(_ == 42) == Some(42))
+  assert (Some(41).filter(_ == 42) == None)
+  assert ((None: Option[Int]).filter(_ == 42) == None)
+
+  // Exercise 8
+  //assert (ExercisesOption.variance (List(42,42,42)) == Some(0.0))
+  //assert (ExercisesOption.variance (List()) == None)
+
+
+  // Exercise 9
+  assert (ExercisesOption.map2 (Some(42),Some(7)) (_ + _) == Some(49))
+  assert (ExercisesOption.map2 (Some(42),None) (_ + _) == None)
+  assert (ExercisesOption.map2 (None: Option[Int],Some(7)) (_ + _) == None)
+  assert (ExercisesOption.map2 (None: Option[Int],None) (_ + _) == None)
+
+  // Exercise 10
+  assert (ExercisesOption.sequence (List(Some(1), Some(2), Some(42))) == Some(List(1,2,42)))
+  assert (ExercisesOption.sequence (List(None,    Some(2), Some(42))) == None)
+  assert (ExercisesOption.sequence (List(Some(1), None,    Some(42))) == None)
+  assert (ExercisesOption.sequence (List(Some(1), Some(2), None    )) == None)
+
+  // Exercise 11
+  def f (n: Int) :Option[Int] = if (n%2 == 0) Some(n) else None
+  assert (ExercisesOption.traverse (List(1,2,42)) (Some(_)) == Some(List(1,2,42)))
+  assert (ExercisesOption.traverse (List(1,2,42)) (f) == None)
+
+}
